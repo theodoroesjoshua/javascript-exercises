2d279202b516eed94049878a8580bea5e2a0b988
Joshua <theodoroesjoshua@gmail.com>
2022-06-28 20:09:50 +0200

    Finish exercise 11 and 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
+function countAge(person) {
+  let age = 0;
+  if ('yearOfDeath' in person) {
+    age = person.yearOfDeath - person.yearOfBirth;
+  } else {
+    age = new Date().getFullYear() - person.yearOfBirth;
+  }
+  return age;
+}
 
-};
+const findTheOldest = function(people) {
+  let initOldest = 
+  {
+    yearOfBirth: 1000, 
+    yearOfDeath: 1000,
+  };
+
+  return people.reduce((oldest, person) => {
+    return countAge(person) > countAge(oldest) ? person : oldest; 
+  }, initOldest)
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
