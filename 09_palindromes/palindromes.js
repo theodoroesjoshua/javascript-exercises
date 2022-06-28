6951697e6e755348750c435fc981616dc6bc0020
Joshua <theodoroesjoshua@gmail.com>
2022-06-28 19:38:36 +0200

    Finish palindromes and fibonacci exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,47 @@
-const palindromes = function () {
+function isLetter(str) {
+    return str.length === 1 && str.match(/[a-z]/i);
+  }
 
+function cleanupString(string) {
+    const lowercaseString = string.toLowerCase();
+    const oldLength = lowercaseString.length;
+    let cleanString = '';
+    let oldIndex = 0;
+    let newIndex = 0;
+    while (oldIndex < oldLength) {
+        if (isLetter(lowercaseString.charAt(oldIndex))) {
+            cleanString += lowercaseString.charAt(oldIndex);
+            newIndex++;
+            oldIndex++;
+        }
+        else {
+            oldIndex++;
+        }
+    }
+    return cleanString;
+}
+
+function reverseString(string) {
+    let reversedString = '';
+    let oldIndex = string.length - 1;
+    let newIndex = 0;
+    while (oldIndex >= 0) {
+        reversedString += string.charAt(oldIndex);
+        newIndex++;
+        oldIndex--;
+    }
+    return reversedString;
 };
+
+const palindromes = function (string) {
+    const cleanString = cleanupString(string);
+    const reversedString = reverseString(cleanString);
+    return (cleanString === reversedString);
+};
+
+palindromes('racecar!');
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  if (typeof(num) === 'string') {
+    num = parseInt(num);
+  }
+  if (num <= 0) return 'OOPS';
+  if (num == 1) return 1;
 
+  let firstNum = 1;
+  let secondNum = 1;
+  let helperVariable = 0;
+  let order = 2;
+
+  while (num > order) {
+    helperVariable = secondNum;
+    secondNum = firstNum + secondNum;
+    firstNum = helperVariable;
+    order++;
+  }
+  return secondNum;  
 };
 
 // Do not edit below this line
